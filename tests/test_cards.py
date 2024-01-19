185ba0e4d1ec2edd308c2c378ea1676c06640ab0
Jonathan Stokes <stokesj84@gmail.com>
2024-01-19 07:39:05 -0800

    fixed lint errors in test_cards.py

--- tests/test_cards.py
+++ tests/test_cards.py
@@ -4,13 +4,15 @@ from collections import defaultdict
 
 from gops.cards import Card, CardStack, SuitCards, Hand
 
-CLOSE=0.00001
+CLOSE = 0.00001
+
 
 def get_values(cards):
     values = []
     for card in cards:
         values.append(card.value)
     return values
+
 
 def test_Card():
     card_1 = Card("Clubs", "A")
@@ -23,9 +25,9 @@ def test_Card():
     assert card_1.get_num_value() == 14
 
     card_1._val = None
-    assert card_1.get_num_value() == None
-
+    assert card_1.get_num_value() is None
     card_1.display()
+
 
 def test_CardStack():
     cards = [Card("Clubs", "A"), Card("Clubs", 2), Card("Clubs", 3), Card("Clubs", 4), Card("Clubs", 5)]
@@ -41,11 +43,11 @@ def test_CardStack():
 
     # test card in stack
     in_stack, card_loc = stack_1.card_in_stack("Spades", "A")
-    assert in_stack == False
+    assert in_stack is False
     assert card_loc == -1
 
     in_stack, card_loc = stack_1.card_in_stack("Clubs", 4)
-    assert in_stack == True
+    assert in_stack is True
     assert card_loc == 3
 
     # test shuffle
@@ -53,23 +55,23 @@ def test_CardStack():
     attempts = 5
     for x in range(attempts):
         stack_1.shuffle()
-        shuffled_values =  get_values(stack_1._order)
+        shuffled_values = get_values(stack_1._order)
 
         for y in range(stack_1.card_count()):
             if values[y] != shuffled_values[y]:
                 same = False
-                break    
+                break
     assert not same
 
     stack_1.display_cards()
+
 
 def test_SuitCards():
 
     suit = SuitCards("Clubs")
     assert suit.card_count() == 13
-
-    card = suit.draw()
     assert suit.card_count() == 12
+
 
 def test_Hand():
 
@@ -77,26 +79,26 @@ def test_Hand():
 
     # test select card
     card = hand.select_card("Spades", 2)
-    assert card == None
+    assert card is None
 
     card = hand.select_card("Clubs", 2)
     in_stack, card_loc = hand.card_in_stack("Clubs", 2)
     assert card.value() == 2
     assert card.suit() == "Clubs"
-    assert in_stack == False
+    assert in_stack is False
     assert card_loc == -1
 
     # test select random card
     random_card = False
     hand_copy = copy.deepcopy(hand)
-    while hand.empty() == False:
+    while hand.empty() is False:
         card_1 = hand.select_random_card()
         card_2 = hand_copy.select_random_card()
-     
+
         if (card_1.value() != card_2.value()) or (card_1.suit() != card_2.suit()):
             random_card = True
             break
-    assert random_card == True
+    assert random_card is True
 
     hand.display_cards()
     for x in range(5):
